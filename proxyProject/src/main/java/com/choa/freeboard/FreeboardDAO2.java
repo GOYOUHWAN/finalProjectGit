e7d323fe0b6fdc4854e4927ef98a7803d84e867e
user1 <user1@KH1>
2016-12-02 10:06:02 +0900

    1202오전 다은. freeboard manager추가

---
+++ proxyProject/src/main/java/com/choa/freeboard/FreeboardDAO2.java
@@ -0,0 +1,23 @@
+package com.choa.freeboard;
+
+
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class FreeboardDAO2 {
+
+   @Autowired
+   private SqlSession sqlSession;
+   private String namespace = "MyFreeboardMapper.";
+   
+   public List<FreeboardDTO> myBoardList(String id){
+      return sqlSession.selectList(namespace+"myBoardList", id);
+   }
+   
+   
+}
